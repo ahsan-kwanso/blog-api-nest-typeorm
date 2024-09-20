1dbf11c804f12c36a600f40289f9b38195452f63
ahsan-kwanso <m.ahsan@kwanso.com>
2024-09-20 15:48:35 +0500

    get instance of config service and then use that

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from './utils/http-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService); // Get the ConfigService instance
+  const port = configService.get<number>('PORT') || 3000; // Retrieve the port, fallback to 3000 if not defined
+
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+
+  await app.listen(port); // Use the dynamic port
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
