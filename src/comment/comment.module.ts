50d5b6ce96385af5ff1e48f38481d86d7fcb4cbd
ahsan-kwanso <m.ahsan@kwanso.com>
2024-10-01 12:42:08 +0500

    used post service by importing post module instead of using post Repo

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from 'src/comment/comment.entity';
-import { Post } from 'src/post/post.entity';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { PostModule } from 'src/post/post.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment, Post])],
+  imports: [TypeOrmModule.forFeature([Comment]), PostModule],
   providers: [CommentService],
   controllers: [CommentController],
 })
